cf0deba77c2858b0c1dd86605a30e6a9afb0b8db
cwvilain <cwvilain@gmail.com>
2018-12-14 16:47:52 -0500

    Add rules for validating restaurant relationships
    Add validation for RestaurantRelationships --
    - Relationship must contain exactly one rule (ie, cannot contain both
    count and required recipe, nor can both these fields be null)
    - Count must be greater than one but cannot exceed the total count of
    recipes for a given restaurant
    - Required recipe ID must reference a real recipe
    
    NOTE -- realized that it may make sense to refactor this into two tables
    since parent_id can be deduced from recipe.

--- app/models/restaurant_relationship.rb
+++ app/models/restaurant_relationship.rb
@@ -3,5 +3,38 @@ class RestaurantRelationship < ApplicationRecord
   belongs_to :child, class_name: "Restaurant"
   validates :parent_id, presence: true
   validates :child_id, presence: true
+  validate :validate_recipe_count
+  validate :validate_recipe_id
+  validate :validate_contains_exactly_one_rule
+
+  private
+
+    def validate_recipe_count
+      if !required_count.nil?
+        if required_count < 1
+          errors.add(:required_count, "is less than 1")
+        end
+        if required_count > parent.recipes.count
+          errors.add(:required_count, "is greater than the number of recipes for the parent restaurant")
+        end
+      end
+    end
+
+    def validate_recipe_id
+      if !required_recipe_id.nil?
+        if !Recipe.find_by(id: required_recipe_id)
+          errors.add(:required_recipe_id, "does not exist")
+        end
+      end
+    end
+
+    def validate_contains_exactly_one_rule
+      if (required_recipe_id.nil? && required_count.nil?)
+        errors.add(:base, :required_recipe_id_and_recipe_count_blank, message: "required_recipe_id or recipe_count must be present")
+      elsif (!required_recipe_id.nil? && !required_count.nil?)
+        errors.add(:base, :required_recipe_id_and_recipe_count_present, message: "rule cannot contain both recipe_count and required_recipe_id")
+      end
+    end
+
 
 end
--- test/models/restaurant_relationship_test.rb
+++ test/models/restaurant_relationship_test.rb
@@ -7,15 +7,44 @@ class RestaurantRelationshipTest < ActiveSupport::TestCase
   def setup
     @seafood = restaurants(:seafood)
     @continental = restaurants(:steak)
-    @clams = @seafood.recipes.build(name: "Fried clams")
-    @salmon = @continental.recipes.build(name: "Grilled salmon")
-    @rule_number = RestaurantRelationship.new(parent_id: @seafood.id, child_id: @continental.id, required_count: 2)
+    @scallops = recipes(:scallops)
+    @salmon = recipes(:salmon)
+    @surfturf = recipes(:surfturf)
+    @rule_number = RestaurantRelationship.new(parent_id: @seafood.id, child_id: @continental.id, required_count: 1)
+    @rule_recipe = RestaurantRelationship.new(parent_id: @seafood.id, child_id: @continental.id, required_recipe_id: @scallops.id)
   end
 
   test "rule number should be valid" do
     assert @rule_number.valid?
   end
 
-  #test "rule number should"
+  test "rule number cannot be <1" do
+    @rule_number.required_count = -1
+    assert_not @rule_number.valid?
+  end
+
+  test "rule number cannot be greater than parent recipes count" do
+    @rule_number.required_count = 3
+    assert_not @rule_number.valid?
+  end
+
+  test "rule recipe should be valid" do
+    assert @rule_recipe.valid?
+  end
+
+  test "rule recipe must reference a real recipe" do
+    @rule_recipe.required_recipe_id = 1000
+    assert_not @rule_recipe.valid?
+  end
+
+  test "rule must contain one of required_recipe_id or required_count" do
+    @rule_recipe.required_recipe_id = nil
+    assert_not @rule_recipe.valid?
+  end
+
+  test "rule must not contain both of required_recipe_id or required_count" do
+    @rule_number.required_recipe_id = @salmon.id
+    assert_not @rule_number.valid?
+  end
 
 end
